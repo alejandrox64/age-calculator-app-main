f6ada53eb6014cd34f91add696badcde4cc10464
Alejandro Rojas <ale402152@gmail.com>
2023-07-06 14:53:08 -0300

    finish the proyect

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,27 @@
 const birthDay = document.getElementById("date-day");
-const birthMont = document.getElementById("date-month");
+const birthMonth = document.getElementById("date-month");
 const birthYear = document.getElementById("date-year");
 
-const button = document.querySelectorAll(".button");
+const button = document.querySelector(".button");
 
-const year = document.querySelectorAll("year");
-const month = document.querySelectorAll("month");
-const day = document.querySelectorAll("day");
+const year = document.querySelector(".year");
+const month = document.querySelector(".months");
+const day = document.querySelector(".days");
+
+button.addEventListener("click", () => {
+    const birthDate = new Date(
+        birthYear.value,
+        birthMonth.value - 1,
+        birthDay.value
+      );
+    
+      const currentDate = new Date();
+    
+      const ageInMilliseconds = currentDate - birthDate;
+      const ageDate = new Date(ageInMilliseconds);
+    
+      year.textContent = Math.abs(ageDate.getUTCFullYear() - 1970);
+      month.textContent = ageDate.getUTCMonth();
+      day.textContent = ageDate.getUTCDate();
+})
 
