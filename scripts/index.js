b975f2417711e54fa38256488b49ec6449851884
Alejandro Rojas <ale402152@gmail.com>
2023-07-05 22:08:01 -0300

    initializing logic js from the page

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const birthDay = document.getElementById("date-day");
+const birthMont = document.getElementById("date-month");
+const birthYear = document.getElementById("date-year");
+
+const button = document.querySelectorAll(".button");
+
+const year = document.querySelectorAll("year");
+const month = document.querySelectorAll("month");
+const day = document.querySelectorAll("day");
+
